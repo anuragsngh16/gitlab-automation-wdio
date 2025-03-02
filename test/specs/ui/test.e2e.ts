cfd203dba12f1191564816ba6e591323b567fa5e
anurag <singh.anurag.ext@siemens-energy.com>
2025-03-02 17:56:30 +0530

    added docs

--- test/specs/ui/test.e2e.ts
+++ test/specs/ui/test.e2e.ts
@@ -8,6 +8,20 @@ import { testCreateAndSearchLabelFunctionality } from './testLabelCreation';
 import { testSettingPreferencesFunctionality } from './testPreferences';
 import { testConfigureIssueBaordFunctionality } from './testConfigureIssueBoard';
 
+/**
+ * Executes a series of UI tests for GitLab application.
+ * 
+ * This script performs the following actions:
+ * - Logs into the GitLab application using valid credentials.
+ * - Tests the functionality of the GitLab homepage.
+ * - Tests the project creation process, including creating a blank project.
+ * - Tests the creation and search functionality of labels.
+ * - Configures the issue board by adding labels and changing board positions.
+ * - Tests user preference settings, including theme and appearance changes.
+ * 
+ * The script utilizes organization-specific modules and data for testing.
+ */
+
 before('login with valid credentials', async () => {
     await browser.url('https://gitlab.com/users/sign_in');
     await browser.maximizeWindow();
